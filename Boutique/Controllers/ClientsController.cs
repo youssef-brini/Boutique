f4f206defb51bc14f7b518e00e9136ea31b13b2b
youssef <youssef.brini@enis.tn>
2021-11-27 22:09:50 -0800

    Login employe and Client with Roles

--- Boutique/Controllers/ClientsController.cs
+++ Boutique/Controllers/ClientsController.cs
@@ -66,7 +66,7 @@ namespace Boutique.Controllers
 
                 if (result.Succeeded)
                 {
-
+                    await _userManager.AddToRoleAsync(user, "Client");
                     await _signInManager.SignInAsync(user, isPersistent: false);
 
                     return RedirectToAction("index", "Home");
--- Boutique/Controllers/CommandesController.cs
+++ Boutique/Controllers/CommandesController.cs
@@ -54,7 +54,7 @@ namespace Boutique.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CommandeId,dateCommande,dateLivraison")] Commande commande)
+        public async Task<IActionResult> Create([Bind("CommandeId,dateCommande,dateLivraison,CommandeProductId,FournisseurId")] Commande commande)
         {
             if (ModelState.IsValid)
             {
